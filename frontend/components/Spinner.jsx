67c6fbdd5db527a7b5d23de08938e53da27a638c
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-04-28 16:20:05 +0500

    last commit

--- frontend/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Spinner = () => {
-  return (
-    <div className="animate-ping rounded-full h-16 w-16 m-8 bg-sky-600"></div>
-  );
-};
-
-export default Spinner;
